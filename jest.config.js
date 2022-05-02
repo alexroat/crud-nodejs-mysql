071c80abe78792c68a5248b1eadca30e41935570
Alessandro Roat <alexroat@gmail.com>
2022-05-03 00:06:39 +0200

    code coverage configurarion

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    coverageThreshold: {
+      global: {
+          branches: 90,
+          functions: 15,
+          lines: 50,
+          statements: 50,
+      },
+    },
+  };
